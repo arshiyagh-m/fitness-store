b17778bfd3bee340fc93ae8b9488b246cf0e7e8a
Arshiya ghanbari <246164980+arshiyagh-m@users.noreply.github.com>
2026-09-21 02:17:09 +0330

    Create authRoutes.js

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,10 @@
+// filepath: server/src/routes/authRoutes.js
+import express from 'express';
+import { registerUser, authUser } from '../controllers/authController.js';
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', authUser);
+
+export default router;
